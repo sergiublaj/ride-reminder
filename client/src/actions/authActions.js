339b6e77286e1c6299fc980c292b020305898122
sergiumr <s3rgiumr@gmail.com>
2021-06-04 22:41:35 +0300

    added auth actions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -8,6 +8,45 @@ import {
 
 export const loadUser = async () => {
 	if (localStorage.token) {
-		setAuthToken;
+		setAuthToken(localStorage.token);
+	}
+
+	try {
+		const res = await axios.get("/api/auth");
+		const data = res.data;
+
+		dispatch({
+			type: USER_LOADED,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: AUTH_ERROR,
+		});
+	}
+};
+
+export const loginUser = async (formData) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	try {
+		const res = await axios.post("/api/auth", formData, config);
+		const data = res.data;
+
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: data,
+		});
+
+		loadUser();
+	} catch (error) {
+		dispatch({
+			type: LOGIN_FAIL,
+			payload: error.response.data.msg,
+		});
 	}
 };
