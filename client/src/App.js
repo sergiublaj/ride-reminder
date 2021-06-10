a9f22f946e63e9e144bbce2ef891fed3ee070e49
sergiumr <s3rgiumr@gmail.com>
2021-06-10 22:05:33 +0300

    changed state persisto

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store, persistor } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
 import PrivateRoute from "./components/routing/PrivateRoute";
-
+import { loadUser } from "./actions/authActions";
+import { connect } from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
@@ -16,31 +14,30 @@ import About from "./components/pages/About";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
-function App() {
+function App({ loadUser }) {
 	useEffect(() => {
 		M.AutoInit();
+		loadUser();
+
+		// eslint-disable-next-line
 	}, []);
 
 	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<Router>
-					<Fragment>
-						<Navbar />
-						<div className={"container"}>
-							<Switch>
-								<PrivateRoute exact path="/" component={Home} />
-								<Route exact path="/login" component={Login} />
-								<Route exact path="/register" component={Register} />
-								<Route exact path="/about" component={About} />
-								<Route component={NotFound} />
-							</Switch>
-						</div>
-					</Fragment>
-				</Router>
-			</PersistGate>
-		</Provider>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<div className={"container"}>
+					<Switch>
+						<PrivateRoute exact path="/" component={Home} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/about" component={About} />
+						<Route component={NotFound} />
+					</Switch>
+				</div>
+			</Fragment>
+		</Router>
 	);
 }
 
-export default App;
+export default connect(null, { loadUser })(App);
--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,4 @@
 import { createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
@@ -9,20 +7,10 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const persistConfig = {
-	key: "root",
-	storage,
-	whitelist: ["auth"],
-};
-
-const persistReduxReducer = persistReducer(persistConfig, rootReducer);
-
 const store = createStore(
-	persistReduxReducer,
+	rootReducer,
 	initialState,
 	composeWithDevTools(applyMiddleware(...middleware))
 );
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
