0fbf0d47a70152af82db5653a9cfad541e07bff9
sergiumr <s3rgiumr@gmail.com>
2021-07-05 22:10:59 +0300

    added weather api

--- client/src/components/layout/Weather.js
+++ client/src/components/layout/Weather.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { APIkey } from "../../config";
 
-function Weather() {
-	return <div className="card bg-dark">Weather</div>;
+function Weather({ rideState: { loading, rides } }) {
+	let city = "Bucuresti";
+
+	const [temp, setTemp] = useState(0);
+	const [icon, setIcon] = useState("");
+
+	if (!loading && rides !== null && rides.length !== 0) {
+		city = rides[0].start;
+	}
+
+	const fetchWeather = async () => {
+		try {
+			const res = await fetch(
+				`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
+			);
+			const data = await res.json();
+
+			setTemp(Math.round(data.main.temp - 273));
+			setIcon(`https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+		} catch (error) {
+			if (error.message === "city not found") {
+				city = "Bucuresti";
+			}
+		}
+	};
+
+	useEffect(() => {
+		fetchWeather();
+
+		// eslint-disable-next-line
+	}, [city]);
+
+	return (
+		<div className="card bg-dark">
+			<div className="row">
+				<div className="col s6">
+					<span>City: {city}</span> <br />
+					<span>Temperature: {temp} °C</span> <br />
+				</div>
+				<div className="col s6 center-align">
+					<img
+						alt=""
+						src={icon}
+						width="70"
+						height="225"
+						style={{ margin: "-3em" }}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Weather;
+const mapStateToProps = (state) => ({
+	rideState: state.ride,
+});
+
+export default connect(mapStateToProps, null)(Weather);
---
+++ client/src/config.js
@@ -0,0 +1,1 @@
+export const APIkey = "14be998cd92790d20f23fb5370d644af";
